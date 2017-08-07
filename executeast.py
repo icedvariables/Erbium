4acb7df0ca0e6025df80b9228936ffe959249e53
icedvariables <macodeblacko@gmail.com>
2017-08-07 17:16:55 +0100

    Added expression evaluation framework

--- executeast.py
+++ executeast.py
@@ -1,8 +1,11 @@
 class ExecuteAst:
     def __init__(self, ast):
         self.ast = ast
-        self.instructions = {
+        self.statements = {
             "assign": self.statementAssign
+        }
+        self.expressions = {
+            "int": self.expressionInt
         }
         self.variablesByScope = {"global":{}}
         self.currentScope = "global"
@@ -16,15 +19,27 @@ class ExecuteAst:
     def executeStatement(self, statement):
         name = statement[0]
         args = statement[1:]
-        statementFunction = self.instructions.get(name, self.error)
+        statementFunction = self.statements.get(name, self.error)
 
-        print "Executing '" + name + "' with args: " + str(args)
+        print "Executing statement '" + name + "' with args: " + str(args)
 
         statementFunction(*args)
 
+    def executeExpression(self, expression):
+        name = expression[0]
+        args = expression[1:]
+        expressionFunction = self.expressions.get(name, self.error)
+
+        print "Evaluating expression '" + name + "' with args: " + str(args)
+
+        return expressionFunction(*args)
+
     def statementAssign(self, name, value):
         scope = self.variablesByScope[self.currentScope]
-        scope[name] = value
+        scope[name] = self.executeExpression(value)
+
+    def expressionInt(self, value):
+        return int(value)
 
     def error(*args):
         print "ERROR"
